c8d684024dbdfe3986785a2c8124e4a5e4a6c24b
YANETH1009 <yecelycalixto@gmail.com>
2022-12-05 22:14:24 -0500

    Agregando archivo votaciones

---
+++ src/main/java/com/example/demo/VotacionesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacionesApplication.class, args);
+	}
+
+}
